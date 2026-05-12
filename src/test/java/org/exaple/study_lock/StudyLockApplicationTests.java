a33fc37f7424a4d9c4f0e57821fb074124b74a32
choi si won <qm0430@naver.com>
2026-05-12 17:18:56 +0900

    initial commit

---
+++ src/test/java/org/exaple/study_lock/StudyLockApplicationTests.java
@@ -0,0 +1,13 @@
+package org.exaple.study_lock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyLockApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
